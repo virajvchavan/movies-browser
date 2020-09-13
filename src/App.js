20e55305711767184bc438b670ddeb47d5c7a087
Viraj Chavan <virajc@live.com>
2020-09-13 16:59:10 +0530

    Initialize the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import './bulma.css';
 import './App.css';
 
+const IMDB_ENDPINT = "https://api.themoviedb.org/3/";
+const API_KEY = "9be5962f75cabd26c04eb4443674e0d2";
+
 function App() {
+  const [categories, setCategories] = useState({});
+
+  useEffect(() => {
+    async function getCategories() {
+      let response = await fetch(IMDB_ENDPINT + "genre/movie/list?api_key=" + API_KEY);
+      let categoriesJson = await response.json();
+      console.log(categoriesJson);
+    }
+    getCategories();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Movie List</h1>
+
     </div>
   );
 }
