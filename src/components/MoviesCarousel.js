49479669c86002c7fa95751df51e3f0a96ee1e05
Viraj Chavan <virajc@live.com>
2020-09-14 11:44:04 +0530

    Add better gradient for carousel

--- src/components/MoviesCarousel.js
+++ src/components/MoviesCarousel.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import * as Constants from './../constants.js';
+import { IMG_ENDPOINT } from './../constants.js';
 
 function MoviesCarousel (props) {
     const {carouselImages} = props;
     if (carouselImages.length > 0) {
         return (
             <div className="columns carousel_container">
-                <div className="column is-one-third">
+                <div className="carousel_overlay"></div>
+                <div className="column is-one-third movies-title-column">
                     <div className="title movies-title">Movies Browser</div>
                 </div>
                 <Carousel
@@ -27,7 +28,7 @@ function MoviesCarousel (props) {
                     {carouselImages.map((imagePath, index)=> {
                         return (
                             <div key={index}>
-                                <img src={Constants.IMG_ENDPOINT + imagePath} />
+                                <img src={IMG_ENDPOINT + imagePath} />
                             </div>
                         )
                     })}
